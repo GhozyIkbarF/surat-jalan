b4d1cdc784598f77311ae105c59885ae31fff4f5
MuhammadAsharul <masharul51@gmail.com>
2022-11-09 14:37:46 +0700

    plisss

---
+++ resources/views/includes/tombol.blade.php
@@ -0,0 +1,11 @@
+<div class="btn-group" role="group">
+    <button type="button" class="btn btn-md btn-primary">
+      <i class="mdi mdi-printer"></i>
+    </button>
+    <a href="#" class="btn btn-md btn-warning" data-id="{{ $pegawai->id }}">
+      <i class="mdi mdi-tooltip-edit"></i>
+    </a>
+    <a href="#" class="btn btn-md btn-danger" data-id="{{ $pegawai->id }}">
+      <i class="mdi mdi-delete"></i>
+    </a>
+  </div>
